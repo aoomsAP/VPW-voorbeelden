8e5696add4af22b52b62ae95919e0954c90e59c9
Alki Ooms <alki.ooms@student.ap.be>
2024-02-12 06:47:45 +0100

    restructure folder

---
+++ Toelatingsproef/C#/VPW/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+
+class Program
+{
+    private static TextReader stdin = System.Console.In;
+    private static TextWriter stdout = System.Console.Out;
+
+    private static void Main(string[] args)
+    {
+        // input
+        int numLists = Convert.ToInt16(stdin.ReadLine());
+        int listLength = 0;
+        int listIndex = -1;
+        List<int>[] lists = new List<int>[numLists];
+
+        string input;
+        while ((input = stdin.ReadLine()) != "")
+        {
+            int n = Convert.ToInt16(input);
+
+            if (listLength == 0)
+            {
+                listIndex++;
+                listLength = n;
+            }
+            else
+            {
+                if (lists[listIndex] == null) lists[listIndex] = new List<int>();
+                lists[listIndex].Add(n);
+                listLength--;
+            }
+        }
+
+        // output
+        for (int i = 0; i < lists.Count(); i++)
+        {
+            stdout.WriteLine($"{i + 1} {lists[i].Min()} {lists[i].Max()}");
+        }
+    }
+}
