96b4439c28c642b6e9f2f97cb975fd9ef40f1a1d
Alki Ooms <alki.ooms@student.ap.be>
2024-02-22 03:30:29 +0100

    refactored with lazylines - doesn't work in VSC but works in online js compiler

--- Opgaves 2023/Aanwerving/aanwerving.js
+++ Opgaves 2023/Aanwerving/aanwerving.js
@@ -1,44 +1,67 @@
-const fs = require("fs");
-const input = fs.readFileSync("wedstrijd.txt", "utf8");
+const ll = require('lazylines');
 
-const lines = input.split("\n").slice(1).filter(x => x != "");
+process.stdin.resume();
+const input = new ll.LineReadStream(process.stdin);
 
-// parsing
+let testsAmount = 0;
+let programmers = null;
+let testRound = null;
 let tests = [];
-for (let i = 0; i < lines.length; i += 2) {
-    tests.push({
-        programmers: +lines[i].split(" ")[0], // 1-1000
-        testRound: +lines[i].split(" ")[1], // 1-2000
-        scores: lines[i + 1].split(" ").map(x => +x), // 1-2000
-    })
-}
 
-// calculation
-tests.forEach((test,index) => {
-    // get high score
-    let highestScore = 0;
-    for (let i = 0; i < test.testRound; i++) {
-        if (test.scores[i] > highestScore) highestScore = test.scores[i];
+input.on('line', (l) => {
+    let line = ll.chomp(l);
+
+    if (testsAmount == 0) {
+        testsAmount = +line;
+    }
+    else if (programmers == null && testRound == null) {
+        programmers = line.split(" ").map(x => +x)[0];
+        testRound = line.split(" ").map(x => +x)[1];
+    }
+    else {
+        let scores = line.split(" ").map(x => +x);
+        // console.log("programmers",programmers);
+        // console.log("scores",scores.length);
+        tests.push({
+            programmers: programmers, // 1-1000
+            testRound: testRound, // 1-2000
+            scores: scores // 1-2000
+        });
+
+        programmers = null; testRound = null;
+        testsAmount--;
     }
 
-    let programmer;
-    for (let i = test.testRound; i < test.scores.length; i++) {
-        // hire first programmer that equals/surpasses highest score
-        if (programmer === undefined && test.scores[i] >= highestScore) {
-            programmer = {
-                index: i,
-                score: test.scores[i],
-            }
-        }
-        // if no programmer hired by the end, hire last one
-        if (programmer === undefined && i === test.scores.length-1) {
-            programmer = {
-                index: i,
-                score: test.scores[i],
-            }
-        }
-    }
+    if (testsAmount == 0) process.exit();
+});
 
-    // index (1) + index programmer (1) + score
-    console.log(`${index+1} ${programmer.index+1} ${programmer.score}`);
+process.on('exit', () => {
+    tests.forEach((test, i) => {
+        // get high score
+        let highestScore = 0;
+        for (let i = 0; i < test.testRound; i++) {
+            if (test.scores[i] > highestScore) highestScore = test.scores[i];
+        }
+
+        let programmer;
+        for (let i = test.testRound; i < test.scores.length; i++) {
+            // hire first programmer that equals/surpasses highest score
+            if (programmer === undefined && test.scores[i] >= highestScore) {
+                programmer = {
+                    index: i,
+                    score: test.scores[i],
+                }
+            }
+            // if no programmer hired by the end, hire last one
+            if (programmer === undefined && i === test.scores.length - 1) {
+                programmer = {
+                    index: i,
+                    score: test.scores[i],
+                }
+            }
+        }
+
+        // index (1) + index programmer (1) + score
+        console.log(`${i + 1} ${programmer.index + 1} ${programmer.score}`);
+    });
 });
